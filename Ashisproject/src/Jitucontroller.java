12a8687b8a1e01330dcce8ebd5ac365bb0d5645b
dev1 <dev@gmail.com>
2020-09-29 16:46:49 +0530

    first created commited

--- Ashisproject/src/Jitucontroller.java
+++ Ashisproject/src/Jitucontroller.java
@@ -3,6 +3,7 @@ public class Jitucontroller {
 	public static void main(String[] args) {
 		System.out.println("Ashis project ruuning good");
 		System.out.println("Not branch created");
+		System.out.println("created");
 	}
 
 }
